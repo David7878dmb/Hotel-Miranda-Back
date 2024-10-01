531f2f7d4e36abfee1f84ed6c455c172725eb2e2
David7878 <david7878.dmb@gmail.com>
2024-10-01 11:59:23 +0200

    Fix: BookingRoutes

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -5,18 +5,18 @@ import { authMiddleware } from '../middleware/auth';
 const router = Router();
 
 //Todas las habitaciones 
-router.get('/', authMiddleware, bookingsController.getAllRooms);
+router.get('/', authMiddleware, bookingsController.getAllBookings);
 
 //Habitacion por id
-router.get('/:id', authMiddleware, bookingsController.getRoomById);
+router.get('/:id', authMiddleware, bookingsController.getBookingsById);
 
 //Crear habitacion
-router.get('/', authMiddleware, bookingsController.createRoom);
+router.get('/', authMiddleware, bookingsController.createBookings);
 
 //Actualizar habitación por ID
-router.get('/:id', authMiddleware, bookingsController.updateRoom);
+router.get('/:id', authMiddleware, bookingsController.updateBookings);
 
 //Eliminar habitacion por iD
-router.get('/:id', authMiddleware, bookingsController.delateRoom);
+router.get('/:id', authMiddleware, bookingsController.delateBookings);
 
 export default router;
