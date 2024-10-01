7fe7faf4bc2f2fc5b5482f8fbc1bbbc8e5d86016
David7878 <david7878.dmb@gmail.com>
2024-10-01 11:55:23 +0200

    Feat: RoomRoutes

---
+++ src/routes/roomsRoutes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { roomsController } from '../controllers/roomsController';
+import { authMiddleware } from '../middlewares/auth';
+
+const router = Router();
+
+//Todas las habitaciones 
+router.get('/', authMiddleware, roomsController.getAllRooms);
+
+//Habitacion por id
+router.get('/:id', authMiddleware, roomsController.getRoomById);
+
+//Crear habitacion
+router.get('/', authMiddleware, roomsController.createRoom);
+
+//Actualizar habitación por ID
+router.get('/:id', authMiddleware, roomsController.updateRoom);
+
+//Eliminar habitacion por iD
+router.get('/:id', authMiddleware, roomsController.delateRoom);
+
+export default router;
