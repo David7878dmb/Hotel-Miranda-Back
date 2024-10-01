4662752bc0e6367cf11e48a12916de970ddd1de5
David7878 <david7878.dmb@gmail.com>
2024-10-01 12:09:40 +0200

    Feat:UsersRoutes

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { usersController } from '../controllers/usersControllers';
+import { authMiddleware } from '../middleware/auth';
+
+const router = Router();
+
+//Todas las habitaciones 
+router.get('/', authMiddleware, usersController.getAllUsers);
+    
+//Habitacion por id
+router.get('/:id', authMiddleware, usersController.getUsersById);
+
+//Crear habitacion
+router.get('/', authMiddleware, usersController.createUsers);
+
+//Actualizar habitación por ID
+router.get('/:id', authMiddleware, usersController.updateUsers);
+
+//Eliminar habitacion por iD
+router.get('/:id', authMiddleware, usersController.delateUsers);
+
+export default router;
